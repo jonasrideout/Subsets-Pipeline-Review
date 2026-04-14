3ebabc8ced3a20edc499c84d7c8157289db51b60
jonasrideout <jonas.rideout@gmail.com>
2026-04-14 16:47:08 -0400

    Add OutboundPanel to DiscoveryTab component

--- components/tabs/DiscoveryTab.tsx
+++ components/tabs/DiscoveryTab.tsx
@@ -8,6 +8,7 @@ import { isStale } from "@/lib/flags";
 import { TableCard } from "@/components/Table";
 import PacingTable from "@/components/PacingTable";
 import DealTable from "@/components/DealTable";
+import OutboundPanel from "@/components/OutboundPanel";
 import StatCard from "@/components/StatCard";
 import type { PipelineCounts } from "@/app/page";
 
@@ -139,6 +140,8 @@ export default function DiscoveryTab({
           />
         </TableCard>
       </div>
+      {/* Outbound Activity */}
+      <OutboundPanel />
     </div>
   );
 }
